50b41800154c18e1aa93c37b1e6465a262a935f9
adama diouf <adamadiouf2017@gmail.com>
2024-02-15 21:03:04 -0600

    add

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
-const Dashboard = () => {
+const Dashboard = ({ paymentInfo }) => {
   const { userInfo } = useSelector((state) => state.auth)
   const isNewUser = userInfo?.isNewUser
 
@@ -17,7 +17,13 @@ const Dashboard = () => {
         This is your dashboard where you can view your payments, transactions,
         and other important information.
       </p>
-      {/* Add your dashboard content here */}
+      <div>
+        {paymentInfo && paymentInfo.amount ? (
+          <p>Payment total: {paymentInfo.amount}</p>
+        ) : (
+          <p>No payment information available</p>
+        )}
+      </div>
     </div>
   )
 }
